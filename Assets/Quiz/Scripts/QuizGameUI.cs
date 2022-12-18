b86c7287b2df480faff948aa91ff27e841e233ca
Ivan <ivan.gonzalez.reguera@estudiantat.upc.edu>
2022-12-18 11:38:13 +0100

    first commit

---
+++ Assets/Quiz/Scripts/QuizGameUI.cs
@@ -0,0 +1,246 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class QuizGameUI : MonoBehaviour
+{
+#pragma warning disable 649
+    [SerializeField] private QuizManager quizManager;               //QuizManager script
+    [SerializeField] private CategoryBtnScript categoryBtnPrefab;
+    [SerializeField] private GameObject scrollHolder;
+    [SerializeField] private Text scoreText, timerText;
+    [SerializeField] private List<Image> lifeImageList;
+    [SerializeField] private GameObject gameOverPanel, mainMenu, gamePanel, gameOverGoodPanel, gameLevelCompletedPanel;
+    [SerializeField] private Color correctCol, wrongCol, normalCol, defaultCol; //color of buttons
+    [SerializeField] private Image questionImg;                     //image component to show image
+    [SerializeField] private UnityEngine.Video.VideoPlayer questionVideo;   //to show video (optional)
+    [SerializeField] private AudioSource questionAudio;             //audio source for audio clip
+    [SerializeField] private Text questionInfoText;                 //text to show question
+    [SerializeField] private List<Button> options;                  //options button reference
+#pragma warning restore 649
+
+    private float audioLength;          //store audio length
+    private Question question;          //store current question data
+    private bool answered = false;      //bool to keep track if answered or not
+    private List<string> answers = new List<string>(); 
+    public Text TimerText { get => timerText; }                     //getter
+    public Text ScoreText { get => scoreText; }                     //getter
+    public GameObject GameOverPanel { get => gameOverPanel; }                     //getter
+    public GameObject GameOverGoodPanel { get => gameOverGoodPanel; }    //getter
+    public GameObject GameLevelCompletedPanel { get => gameLevelCompletedPanel; }    //getter
+
+    private void Start()
+    {
+        //add the listener to all the buttons
+        for (int i = 0; i < options.Count; i++)
+        {
+            Button localBtn = options[i];
+            localBtn.onClick.AddListener(() => OnClick(localBtn));
+        }
+        lifeImageList[1].enabled = false;
+        lifeImageList[2].enabled = false;
+        CreateCategoryButtons();
+    }
+
+
+
+    /// Method which populate the question on the screen
+    /// <param name="question"></param>
+    public void SetQuestion(Question question)
+    {
+        //set the question
+        this.question = question;
+        //check for questionType
+        switch (question.questionType)
+        {
+            case QuestionType.TEXT:
+                questionImg.transform.parent.gameObject.SetActive(false);   //deactivate image holder
+                break;
+            case QuestionType.IMAGE:
+                questionImg.transform.parent.gameObject.SetActive(true);    //activate image holder
+                questionVideo.transform.gameObject.SetActive(false);        //deactivate questionVideo
+                questionImg.transform.gameObject.SetActive(true);           //activate questionImg
+                questionAudio.transform.gameObject.SetActive(false);        //deactivate questionAudio
+
+                questionImg.sprite = question.questionImage;                //set the image sprite
+                break;
+            case QuestionType.AUDIO:
+                questionVideo.transform.parent.gameObject.SetActive(true);  //activate image holder
+                questionVideo.transform.gameObject.SetActive(false);        //deactivate questionVideo
+                questionImg.transform.gameObject.SetActive(false);          //deactivate questionImg
+                questionAudio.transform.gameObject.SetActive(true);         //activate questionAudio
+                
+                audioLength = question.audioClip.length;                    //set audio clip
+                StartCoroutine(PlayAudio());                                //start Coroutine
+                break;
+            case QuestionType.VIDEO:
+                questionVideo.transform.parent.gameObject.SetActive(true);  //activate image holder
+                questionVideo.transform.gameObject.SetActive(true);         //activate questionVideo
+                questionImg.transform.gameObject.SetActive(false);          //deactivate questionImg
+                questionAudio.transform.gameObject.SetActive(false);        //deactivate questionAudio
+
+                questionVideo.clip = question.videoClip;                    //set video clip
+                questionVideo.Play();                                       //play video
+                break;
+        }
+
+        questionInfoText.text = question.questionInfo;                      //set the question text
+
+        //suffle the list of options
+        List<string> ansOptions = ShuffleList.ShuffleListItems<string>(question.options);
+        //assign options to respective option buttons
+        for (int i = 0; i < options.Count; i++) {
+            options[i].GetComponentInChildren<Text>().text = ""; //reset
+            options[i].name = "";    //reset
+            options[i].image.color = defaultCol; //set color of button to normal
+            options[i].enabled = false;
+        }
+        for (int i = 0; i < ansOptions.Count; i++)
+        {
+            //set the child text
+            options[i].GetComponentInChildren<Text>().text = ansOptions[i];
+            options[i].name = ansOptions[i];    //set the name of button
+            options[i].image.color = normalCol;
+            options[i].enabled = true;
+        }
+        answered = false;                       
+
+    }
+
+    public void ReduceLife(int remainingLife)
+    {
+        int currentLife = 2 - remainingLife;
+        lifeImageList[currentLife].enabled = false;
+        if (remainingLife != 0) lifeImageList[currentLife + 1].enabled = true;
+
+    }
+
+    /// IEnumerator to repeate the audio after some time
+    IEnumerator PlayAudio()
+    {
+        //if questionType is audio
+        if (question.questionType == QuestionType.AUDIO)
+        {
+            //PlayOneShot
+            questionAudio.PlayOneShot(question.audioClip);
+            //wait for few seconds
+            yield return new WaitForSeconds(audioLength + 0.5f);
+            //play again
+            StartCoroutine(PlayAudio());
+        }
+        else //if questionType is not audio
+        {
+            //stop the Coroutine
+            StopCoroutine(PlayAudio());
+            //return null
+            yield return null;
+        }
+    }
+
+    /// Method assigned to the buttons
+    void OnClick(Button btn)
+    {
+        if (quizManager.GameStatus == GameStatus.PLAYING)
+        {
+            //if answered is false and we picked enough answers
+            answers.Add(btn.name);
+            if (!answered && answers.Count == quizManager.getNumberAnswers())
+            {
+                //set answered true
+                answered = true;
+                //get the bool value
+                bool val = quizManager.Answer(answers);
+
+                
+                //if its true
+                if (val)
+                {
+                    List<string> correct = quizManager.getCorrect();
+                    for (int i = 0; i < options.Count; i++) {
+                        for (int j = 0; j < correct.Count; ++j) {
+                        if (options[i].name == correct[j])  StartCoroutine(BlinkImg(options[i].image));  //Start blinking correct answers
+                        } 
+                    }
+                }
+                else
+                {
+                    //show correct answer
+                    List<string> correct = quizManager.getCorrect();
+                    for (int i = 0; i < options.Count; i++) {
+                        for (int j = 0; j < correct.Count; ++j) {
+                        if (options[i].name == correct[j]) options[i].image.color = correctCol; //set color of button to correct
+                        } 
+                    }
+                    //else set it to wrong color
+                    for (int i = 0; i < answers.Count; i++) { 
+                        for (int j = 0; j < options.Count; ++j) {
+                            if (answers[i] == options[j].name && options[j].image.color != correctCol) options[j].image.color = wrongCol;
+                        }
+                    }
+                }
+                answers.Clear();
+            }
+        }
+    }
+
+
+    /// Method to create Category Buttons dynamically
+    void CreateCategoryButtons()
+    {
+        //we loop through all the available catgories in our QuizManager
+        for (int i = 0; i < quizManager.QuizData.Count; i++)
+        {
+            //Create new CategoryBtn
+            CategoryBtnScript categoryBtn = Instantiate(categoryBtnPrefab, scrollHolder.transform);
+            //Set the button default values
+            categoryBtn.SetButton(quizManager.QuizData[i].categoryName, quizManager.QuizData[i].questions.Count);
+            int index = i;
+            //Add listner to button which calls CategoryBtn method
+            categoryBtn.Btn.onClick.AddListener(() => CategoryBtn(index, quizManager.QuizData[index].categoryName));
+        }
+    }
+
+    //Method called by Category Button
+    private void CategoryBtn(int index, string category)
+    {
+        quizManager.StartGame(index, category); //start the game
+        bool completed =  quizManager.completed();
+        mainMenu.SetActive(false);              //deactivate mainMenu
+        gamePanel.SetActive(true);              //activate game panel
+        if (completed) {
+        quizManager.CompletedLevel();
+        }
+    
+    }
+
+    //this give blink effect [if needed use or dont use]
+    IEnumerator BlinkImg(Image img)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            img.color = Color.white;
+            yield return new WaitForSeconds(0.1f);
+            img.color = correctCol;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    //retry button
+    public void RetryButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    //exit button
+    public void ExitGame() {
+        Application.Quit();
+    }
+    //reset button
+    public void ResetScore() {
+        quizManager.Reset();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
---
+++ Assets/Quiz/Scripts/QuizManager.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class QuizManager : MonoBehaviour
+{
+#pragma warning disable 649
+    //ref to the QuizGameUI script
+    [SerializeField] private QuizGameUI quizGameUI;
+    //ref to the scriptableobject file
+    [SerializeField] private List<QuizDataScriptable> quizDataList;
+    [SerializeField] private float timeInSeconds;
+#pragma warning restore 649
+
+    private string currentCategory = "";
+    private int correctAnswerCount = 0;
+    //questions data
+    private List<Question> questions;
+    //current question data
+    private Question selectedQuestion = new Question();
+    private int gameScore;
+    private int lifesRemaining;
+    private float currentTime;
+    private QuizDataScriptable dataScriptable;
+    private int totalQuestions;
+
+    private GameStatus gameStatus = GameStatus.NEXT;
+
+    public GameStatus GameStatus { get { return gameStatus; } }
+
+    public List<QuizDataScriptable> QuizData { get => quizDataList; }
+
+    public void StartGame(int categoryIndex, string category)
+    {
+        currentCategory = category;
+        correctAnswerCount = 0;
+        gameScore = 0;
+        lifesRemaining = 3;
+        currentTime = timeInSeconds;
+        //set the questions data
+        questions = new List<Question>();
+        dataScriptable = quizDataList[categoryIndex];
+        questions.AddRange(dataScriptable.questions);
+        totalQuestions = questions.Count;
+        //select the question
+        SelectQuestion();
+        gameStatus = GameStatus.PLAYING;
+
+    }
+
+    /// Method used to randomly select the question form questions data
+    private void SelectQuestion()
+    {
+        //get the random number
+        int val = UnityEngine.Random.Range(0, questions.Count);
+        //set the selectedQuestion
+        selectedQuestion = questions[val];
+        //send the question to quizGameUI
+        quizGameUI.SetQuestion(selectedQuestion);
+        questions.RemoveAt(val);
+    }
+
+    private void Update()
+    {
+        if (gameStatus == GameStatus.PLAYING)
+        {
+            currentTime -= Time.deltaTime;
+            SetTime(currentTime);
+        }
+    }
+
+    void SetTime(float value)
+    {
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);                       //set the time value
+        quizGameUI.TimerText.text = time.ToString("mm':'ss");   //convert time to Time format
+
+        if (currentTime <= 0)
+        {
+            //Game Over
+            GameEndBad();
+        }
+    }
+
+    //get correct answer for Selectedquestion
+    public List<string> getCorrect()
+    {
+        return selectedQuestion.correctAns;
+    }
+
+    //get number of answers
+    public int getNumberAnswers()
+    {
+        return selectedQuestion.correctAns.Count;
+    }
+
+    public bool completed() {
+        if (PlayerPrefs.GetInt(currentCategory, 0) == totalQuestions) return true;
+        return false;
+    }
+
+
+    /// Method called to check the answer is correct or not
+    public bool Answer(List<string> selectedOptions)
+    {
+        //set default to false
+        bool correct = false;
+        selectedOptions.Sort();
+        List<string> aux = selectedQuestion.correctAns;
+        aux.Sort();
+        bool eq = true;
+        for (int i = 0; i < aux.Count; ++i) {
+            bool igual = false;
+            for (int j = 0; j < selectedOptions.Count; ++j) {
+                if (aux[i] == selectedOptions[j]) igual = true;
+            }
+            if (igual == false) {
+                eq = false; 
+                break;
+            }
+        }
+
+
+        //if selected answer is similar to the correctAns
+        if (eq)
+        {
+            //Yes, Ans is correct
+            correctAnswerCount++;
+            correct = true;
+            gameScore += 50 * aux.Count;
+            quizGameUI.ScoreText.text = "Puntuació:" + gameScore;
+        }
+        else
+        {
+            //No, Ans is wrong
+            //Reduce Life
+            lifesRemaining--;
+            quizGameUI.ReduceLife(lifesRemaining);
+
+            if (lifesRemaining == 0)
+            {
+                GameEndBad();
+            }
+        }
+
+        if (gameStatus == GameStatus.PLAYING)
+        {
+            if (questions.Count > 0)
+            {
+                //call SelectQuestion method again after 1s
+                Invoke("SelectQuestion", 0.4f);
+            }
+            else
+            {
+                PlayerPrefs.SetInt(currentCategory, correctAnswerCount);
+                if (PlayerPrefs.GetInt(currentCategory, 0) == totalQuestions) GameEndGood();
+                else quizGameUI.RetryButton();
+            }
+        }
+        //return the value of correct bool
+        return correct;
+    }
+
+    //retry button and save prefs
+    public void retryManager() {
+        PlayerPrefs.SetInt(currentCategory, correctAnswerCount);
+        quizGameUI.RetryButton();
+    }
+
+    //game completed bad popup
+    private void GameEndBad()
+    {
+        gameStatus = GameStatus.NEXT;
+        quizGameUI.GameOverPanel.SetActive(true);
+
+        //fi you want to save only the highest score then compare the current score with saved score and if more save the new score
+        //eg:- if correctAnswerCount > PlayerPrefs.GetInt(currentCategory) then call below line
+
+        //Save the score
+        PlayerPrefs.SetInt(currentCategory, correctAnswerCount); //save the score for this category
+    }
+
+    //game completed good popup
+    private void GameEndGood()
+    {
+        gameStatus = GameStatus.NEXT;
+        quizGameUI.GameOverGoodPanel.SetActive(true);
+
+    }
+
+    //level completed popup
+    public void CompletedLevel() {
+       gameStatus = GameStatus.NEXT;
+       quizGameUI.GameLevelCompletedPanel.SetActive(true);
+    }
+
+    //Reset button
+    public void Reset() {
+         for (int i = 0; i < quizDataList.Count; i++)
+        {
+            PlayerPrefs.SetInt(quizDataList[i].categoryName, 0);
+            questions = new List<Question>();
+            dataScriptable = quizDataList[i];
+            questions.AddRange(dataScriptable.questions);
+        }
+    }
+}
+
+
+//Datastructure for storeing the quetions data
+[System.Serializable]
+public class Question
+{
+    public string questionInfo;         //question text
+    public QuestionType questionType;   //type
+    public Sprite questionImage;        //image for Image Type
+    public AudioClip audioClip;         //audio for audio type
+    public UnityEngine.Video.VideoClip videoClip;   //video for video type
+    public List<string> options;        //options to select
+    public List<string> correctAns;     //correct option
+}
+
+[System.Serializable]
+public enum QuestionType
+{
+    TEXT,
+    IMAGE,
+    AUDIO,
+    VIDEO
+}
+
+[SerializeField]
+public enum GameStatus
+{
+    PLAYING,
+    NEXT
+}
